6752162e49881325a67573ce571a101cf9e3debf
fireedwin <ee587023@gmail.com>
2025-12-05 11:44:26 +0800

    arisu is my daughter

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+
+int sub(int a, int b) {
+    return a - b;
+}
+
 int main() {
     printf("=== C Language Calculator ===\n");
-    // Operations placeholders        
-    printf("System Check OK.\n");        
+
+    // Operations placeholders
+    printf("Subtraction: 50 - 30 = %d\n", sub(50, 30));
+
+    printf("System Check OK\n");
     return 0;
 }
