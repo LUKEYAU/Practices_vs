b846fc5211defeb2f53bab02712b9397c477d8c6
fireedwin <ee587023@gmail.com>
2025-12-05 11:49:12 +0800

    but I love azusa

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-int sub(int a, int b) {
-    return a - b;
-}
+int add(int a, int b) { return a + b; }
+int sub(int a, int b) { return a - b; }
 
 int main() {
     printf("=== C Language Calculator ===\n");
 
-    // Operations placeholders
+    printf("Addition: 10 + 20 = %d\n", add(10, 20));
     printf("Subtraction: 50 - 30 = %d\n", sub(50, 30));
 
     printf("System Check OK\n");
