a909540bd068ae6065975522ed896bc979cc0a3a
LukeYau <yaukayuet20031017@gmail.com>
2025-12-05 11:41:58 +0800

    feat: add

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
 int main() {
     printf("=== C Language Calculator ===\n");
-    // Operations placeholders        
-    printf("System Check OK.\n");        
+    
+    // Operations placeholders
+    printf("Addition: 10 + 20 = %d\n", add(10, 20));
+    
+    printf("System Check OK\n");
     return 0;
 }
